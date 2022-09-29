a512e5a683d5a4589a90b62b287d36b7c8360fb8
daregoodness1997 <daregoodness@gmail.com>
2022-09-29 11:29:48 +0100

    header, hero and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+const navigation = [
+  {
+    name: 'Term of Use',
+    href: '#',
+  },
+  {
+    name: 'Privacy Policy',
+    href: '#',
+  },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className='bg-blue-900 py-4 '>
+      <div className='max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8'>
+        <div className='flex justify-center space-x-6 md:order-2'>
+          {navigation.map(item => (
+            <a
+              key={item.name}
+              href={item.href}
+              className='text-white hover:text-gray-500'
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+        <div className='mt-8 md:mt-0 md:order-1'>
+          <p className='text-lg font-bold text-white'>HCI Healthcare</p>
+        </div>
+      </div>
+      <div className='mt-8 md:mt-0 md:order-1'>
+        <p className='text-center text-base text-white'>
+          &copy; 2022 HealtStack
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
